2bcc3b89257b87ee590a7ea3ee4368de4ed163ab
Michael <mike.witk@gmail.com>
2019-04-24 20:41:36 +0000

    query to display dish name and price based on user name

--- db/foodNow_schema.sql
+++ db/foodNow_schema.sql
@@ -2,16 +2,21 @@ CREATE TABLE IF NOT EXISTS dishes {
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(50),
   description VARCHAR(50),
-  price REAL
+  price NUMERIC
 };
 
 CREATE TABLE IF NOT EXISTS orders {
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(50),
-  phone_number INTEGER
+  phone_number BIGINT
 };
 
-CREATE TABLE IF NOT EXISTS dishes_order {
+CREATE TABLE IF NOT EXISTS dishes_orders {
   orders_id INTEGER,
-  dishes_id
+  dishes_id INTEGER
 };
+
+SELECT orders.id, dishes.name, dishes.price
+FROM orders JOIN dishes_orders ON orders.id = orders_id
+JOIN dishes ON dishes_id = dishes.id
+WHERE orders.name = 'Mike';
--- db/seeds/dishes.js
+++ db/seeds/dishes.js
@@ -3,5 +3,17 @@ INSERT INTO dishes(name, description, price) VALUES('dish B', 'desc B', 8.99);
 INSERT INTO dishes(name, description, price) VALUES('dish C', 'desc C', 7.99);
 INSERT INTO dishes(name, description, price) VALUES('dish D', 'desc D', 15.99);
 
-INSERT INTO orders (name, phone_number) VALUES ("Mike", 1111111111);
-INSERT INTO orders (name, phone_number) VALUES ("Carmen", 2222222222);
+INSERT INTO orders(name, phone_number) VALUES('Mike', 1111111111);
+INSERT INTO orders(name, phone_number) VALUES('Carmen', 2222222222);
+
+INSERT INTO dishes_orders(orders_id, dishes_id) VALUES(1, 1);
+INSERT INTO dishes_orders(orders_id, dishes_id) VALUES(1, 2);
+INSERT INTO dishes_orders(orders_id, dishes_id) VALUES(1, 3);
+INSERT INTO dishes_orders(orders_id, dishes_id) VALUES(1, 4);
+INSERT INTO dishes_orders(orders_id, dishes_id) VALUES(2, 1);
+INSERT INTO dishes_orders(orders_id, dishes_id) VALUES(2, 1);
+INSERT INTO dishes_orders(orders_id, dishes_id) VALUES(2, 1);
+INSERT INTO dishes_orders(orders_id, dishes_id) VALUES(2, 1);
+INSERT INTO dishes_orders(orders_id, dishes_id) VALUES(3, 2);
+INSERT INTO dishes_orders(orders_id, dishes_id) VALUES(3, 3);
+INSERT INTO dishes_orders(orders_id, dishes_id) VALUES(3, 4);
