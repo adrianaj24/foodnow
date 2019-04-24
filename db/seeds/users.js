6301d4f94351c3de9a63ebadefc8d29fafc30431
Michael <mike.witk@gmail.com>
2019-04-24 23:51:36 +0000

    migration changed to create dishes, orders, dishes_orders tables

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -4,7 +4,25 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         knex('users').insert({id: 1, name: 'Alice'}),
         knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
+        knex('users').insert({id: 3, name: 'Charlie'}),
+        knex('dishes').insert({id: 1, name: 'Dish A', description: 'desc A', price: 9.9}),
+        knex('dishes').insert({id: 2, name: 'Dish B', description: 'desc B', price: 19.9}),
+        knex('dishes').insert({id: 3, name: 'Dish C', description: 'desc C', price: 29.9}),
+        knex('dishes').insert({id: 4, name: 'Dish D', description: 'desc D', price: 6.9}),
+        knex('orders').insert({id: 1, name: 'Mike', phone_number: 1111111111}),
+        knex('orders').insert({id: 2, name: 'Carmen', phone_number: 2222222222}),
+        knex('dishes_orders').insert({orders_id: 1, dishes_id: 1}),
+        knex('dishes_orders').insert({orders_id: 1, dishes_id: 1}),
+        knex('dishes_orders').insert({orders_id: 1, dishes_id: 1}),
+        knex('dishes_orders').insert({orders_id: 1, dishes_id: 3}),
+        knex('dishes_orders').insert({orders_id: 2, dishes_id: 4}),
+        knex('dishes_orders').insert({orders_id: 2, dishes_id: 2}),
+        knex('dishes_orders').insert({orders_id: 2, dishes_id: 2})
       ]);
     });
+  .then(function() {
+    return Promise.all ([
+      knex('')
+      ])
+    })
 };
