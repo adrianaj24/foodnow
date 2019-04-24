50a47aaf5cb5402aaa7d2d8f4176d34931e5bf52
Michael <mike.witk@gmail.com>
2019-04-24 19:34:36 +0000

    populating table with fake data

--- db/foodNow_schema.sql
+++ db/foodNow_schema.sql
@@ -3,15 +3,15 @@ CREATE TABLE IF NOT EXISTS dishes {
   name VARCHAR(50),
   description VARCHAR(50),
   price REAL
-}
+};
 
 CREATE TABLE IF NOT EXISTS orders {
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(50),
   phone_number INTEGER
-}
+};
 
 CREATE TABLE IF NOT EXISTS dishes_order {
   orders_id INTEGER,
   dishes_id
-}
+};
---
+++ db/seeds/dishes.js
@@ -0,0 +1,7 @@
+INSERT INTO dishes(name, description, price) VALUES('dish A', 'desc A', 9.99);
+INSERT INTO dishes(name, description, price) VALUES('dish B', 'desc B', 8.99);
+INSERT INTO dishes(name, description, price) VALUES('dish C', 'desc C', 7.99);
+INSERT INTO dishes(name, description, price) VALUES('dish D', 'desc D', 15.99);
+
+INSERT INTO orders (name, phone_number) VALUES ("Mike", 1111111111);
+INSERT INTO orders (name, phone_number) VALUES ("Carmen", 2222222222);
