21059b229e969819ee1141a13e6f3d27a71421ee
Michael <mike.witk@gmail.com>
2019-04-25 20:09:24 +0000

    menu item HTML creation from db

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,39 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-      $("<div>").text(user.sum).appendTo($("body"));
-    }
+$(document).ready(function() {
+
+  function createMenuElement (menuData) {
+
+    const dishName = menuData.name;
+    const dishDesc = menuData.description;
+    const dishPrice = menuData.price;
+
+    const $createName = $('<div>').addClass('menu-item-name').text(dishName);
+    const $createDesc = $('<div>').addClass('menu-item-description').text(dishDesc);
+    const $createPrice = $('<div>').addClass('menu-item-price').text(dishPrice);
+
+  const $dish = $('<div>').addClass('menu-item').append($createName)
+                                                .append($createDesc)
+                                                .append($createPrice);
+
+  return $dish
+
+  }
+
+
+  $(() => {
+    $.ajax({
+      method: "GET",
+      url: "/"
+    }).done((foods) => {
+      for(food of foods) {
+        $('.menu-section-title').append(createMenuElement(food));
+      }
+    });
+    // .done( (bill) => {
+    //   $("<div>").text(bill).appendTo($("body"));
+    // })
   });
-  // .done( (bill) => {
-  //   $("<div>").text(bill).appendTo($("body"));
-  // })
-});
+
+
+
+
+})
--- routes/users.js
+++ routes/users.js
@@ -6,22 +6,18 @@ const router  = express.Router();
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
-    knex
-      .select("*")
-      .from("users")
-      .then((results) => {
+    // knex
+    //   .select("*")
+    //   .from("users")
+    //   .then((results) => {
         // res.json(results);
-    });
-    knex.select("orders.name")
-        .sum("dishes.price")
-        .from("orders")
-        .join("dishes_orders","orders.id","orders_id")
-        .join("dishes","dishes_id","dishes.id")
-        .where("orders.name","=","Mike")
-        .groupBy("orders.name")
+    // });
+    knex.select("dishes.name","dishes.description","dishes.price")
+        .from("dishes")
+        .where("dishes.type","=","main")
         .then( (moreResults) => {
           res.json(moreResults)
-          console.log(moreResults);
+          console.log("Result from query: ",moreResults);
         });
   });
 
