7b3ba00c40da4008e87a198211777cbc4f77157a
Michael <mike.witk@gmail.com>
2019-04-25 00:42:59 +0000

    testing query and displaying on main page

--- routes/users.js
+++ routes/users.js
@@ -10,8 +10,19 @@ module.exports = (knex) => {
       .select("*")
       .from("users")
       .then((results) => {
-        res.json(results);
+        // res.json(results);
     });
+    knex.select("orders.id",
+              "dishes.name",
+              "dishes.price")
+        .from("orders")
+        .join("dishes_orders","orders.id","orders_id")
+        .join("dishes","dishes_id","dishes.id")
+        .where("orders.name","=","Mike")
+        .then( (moreResults) => {
+          res.json(moreResults)
+          // console.log(moreResults);
+        });
   });
 
   return router;
