3e17b9f551666e18592f02d3ce5c537130b1397c
Michael <mike.witk@gmail.com>
2019-04-28 16:00:59 +0000

    summary page numbers fixed to 2 decimals:

--- public/scripts/summary.js
+++ public/scripts/summary.js
@@ -1,38 +1,34 @@
-
-
 var total = 0;
 
-  const myObj = JSON.parse(localStorage.getItem('cart'));
+const myObj = JSON.parse(localStorage.getItem('cart'));
 
-  function summaryCart(storage) {
+function summaryCart(storage) {
 
-    const itemId = storage.id;
-    const itemName = storage.name;
-    const itemPrice = storage.price;
-    const itemQty = storage.quantity;
+  const itemId = storage.id;
+  const itemName = storage.name;
+  const itemPrice = storage.price.toFixed(2);
+  const itemQty = storage.quantity;
 
-    $item = `<a class="item1">${itemName}</a> <span class="itemId">${itemId}</span> <span class="quantity"></span> <span class="price">$${itemPrice}</span><div id="quatityButton">Quantity
-                                  <button type="button" id=${itemId} class="sub">-</button>
-                                  <input type="number" id="1" value=${itemQty} min="1" max="3" />
-                                  <button type="button" id=${itemId} class="add">+</button>
-                                  </div>`
+  $item = `<a class="item1">${itemName}</a> <span class="itemId">${itemId}</span> <span class="quantity"></span> <span class="price">$${itemPrice}</span><div id="quatityButton">Quantity
+                                <button type="button" id=${itemId} class="sub">-</button>
+                                <input type="number" id="1" value=${itemQty} min="1" max="3" />
+                                <button type="button" id=${itemId} class="add">+</button>
+                                </div>`
 
-    total += itemPrice * itemQty;
-    return $item
-  }
+  total += itemPrice * itemQty;
+  return $item
+}
 
 
 function renderSummary (cart) {
   for (var item in cart) {
     $('.container').append(summaryCart(cart[item]))
-
   }
 }
 
 function renderTotal(total) {
   $total = `<hr><p>Total <span class="price1" style="color:black"><b>$${total}</b></span></p>`
   $('.container1').append($total)
-
 }
 
 function clearCart(){
@@ -40,41 +36,39 @@ function clearCart(){
   cart = {};
   window.location.reload();
 }
+
 //when the page is about to close
 window.addEventListener('beforeunload', (event) => {
-  // console.log("setting before close");
   localStorage.setItem('cart', null);
   localStorage.setItem('cart', JSON.stringify(cart));
 });
 
 $( function() {
-    const $button = $(".btn");
+  const $button = $(".btn");
 
-    $button.on('click', function (event) {
-      event.preventDefault();
+  $button.on('click', function (event) {
+    event.preventDefault();
 
-      if ( $('#fname').val() === "" || $('#fname').val() === null || $('#pnumber').val() === "" || $('#pnumber').val() === null) {
-        $('.error').slideDown('slow');
-          $('.error').text("Please fill out the form")
-          console.log("I am inside the first function")
-      } else {
-        var data = $('#submitForm').serialize()
-        // console.log(data);
-        $('.error').slideUp('fast');
-        $.ajax({
-          url: '/checkout',
-          type: "POST",
-          data: data,
-          success: function () {
-            console.log("something", data)
-          window.location = "/checkout"
-          }
-
-        })
-      }
-
-    })
+    if ( $('#fname').val() === "" || $('#fname').val() === null || $('#pnumber').val() === "" || $('#pnumber').val() === null) {
+      $('.error').slideDown('slow');
+        $('.error').text("Please fill out the form")
+        console.log("I am inside the first function")
+    } else {
+      var data = $('#submitForm').serialize()
+      // console.log(data);
+      $('.error').slideUp('fast');
+      $.ajax({
+        url: '/checkout',
+        type: "POST",
+        data: data,
+        success: function () {
+          console.log("something", data)
+        window.location = "/checkout"
+        }
+      })
+    }
   })
+})
 
 $(document).ready(function () {
 
